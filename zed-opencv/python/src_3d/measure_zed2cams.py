5bfd609f8c21f9822de1266c59eeb5d5504ad197
于 勝龍 <807028@w2k.isb.co.jp>
2020-11-10 14:17:32 +0900

    ss

--- zed-opencv/python/src_3d/measure_zed2cams.py
+++ zed-opencv/python/src_3d/measure_zed2cams.py
@@ -1,17 +1,12 @@
 import os, sys, time
 import pyzed.sl as sl
-import numpy as np
-# from easydict import EasyDict
-# from enum import IntEnum
 from zed2cams import init,take
 from  multiprocessing import Pool
-import cv2
 
 root_dir =  "dt/output/"
 save_dir_fmt = root_dir + "/cam{}/"
 def __looptake_cbd(arg):
     pass
-
 def __looptake(cam_id):
     menu=init(None, cam_id)
     print(f'menu.save_dir: {menu.save_dir}')
@@ -28,9 +23,6 @@ class multi_take:
   def __init__(self, interval, pron):
     self.pool = Pool(processes=pron)
     self.interval=interval
-    self.menus=[]
-    self.info="take started"
-    self.run=False
   def start(self,work,cam_ids,cbk):
     for i in cam_ids:
         self.pool.apply_async(func=work,
@@ -39,20 +31,7 @@ class multi_take:
         time.sleep(0.1)
         print("process: camera-%d is started!"%(i))
 
-  def looptake_cbd(self,cam_id):
-    menu=init(None, cam_id)
-    self.menus.append(menu)
-    print(f'camera:{cam_id}, menu.save_dir: {menu.save_dir}')
-    j=0
-    while (1):
-        take(menu)
-        print('***************process :%d, loop:%d is started' % (cam_id, j))
-        # self.info = 'process :%d, loop:%d is started' % (cam_id, j)
-        time.sleep(self.interval)
-        j = j + 1
   def terminate(self):
-    for menu in self.menus:
-      menu.zed.cam.close()
     self.pool.close()
     self.pool.terminate()
 def take_data(root_dir):
@@ -63,7 +42,6 @@ def take_data(root_dir):
       if not os.path.exists(root_dir):
         os.makedirs(root_dir, exist_ok=True)
         print(f"{root_dir} is created.")
-
     cameras = sl.Camera.get_device_list()
     cam_ids=[]
     for cam_id, cam in enumerate(cameras):
@@ -74,34 +52,11 @@ def take_data(root_dir):
       if not comm in ['t', 'q']:
         continue
       if comm == 't':
-        mp.run = True
-        # mp.start(mp_f, cam_ids, mp.looptake_cbd)
         mp.start(__looptake, cam_ids, __looptake_cbd)
       elif comm == 'q':
-        mp.run = False
         print('finish script...')
         break
 
-    # print('Please enter command(t: take data, q:quit: ')
-    # im = np.zeros((300, 800), np.uint8)
-    # cv2.imshow('Keypressed', im)
-    #
-    # while(1):
-    #     k = cv2.waitKey(0)
-    #     im_c = im.copy()
-    #     cv2.putText(im_c,
-    #         f'{chr(k)} -> {k}\n%s'%(mp.info),(10, 40),
-    #         cv2.FONT_HERSHEY_SIMPLEX,1,(255, 255, 255), 2)
-    #     cv2.imshow('Keypressed', im_c)
-    #     if k == 27:  # Esc key to stop
-    #         mp.run=False
-    #         print('finish script...')
-    #         break
-    #     elif (k == 116 or  k == 84) and mp.run==False:#t
-    #         mp.run=True
-    #         mp.start(mp_f, cam_ids, mp.looptake_cbd)
-    #     else:
-    #         continue
     mp.terminate()
     sys.exit(1)
 
