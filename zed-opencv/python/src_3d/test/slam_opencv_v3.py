764c1fd9e877db8a3cc0130da720d5221b288e54
于 勝龍 <807028@w2k.isb.co.jp>
2020-12-22 18:36:30 +0900

    ss

--- zed-opencv/python/src_3d/test/slam_opencv_v3.py
+++ zed-opencv/python/src_3d/test/slam_opencv_v3.py
@@ -1,8 +1,11 @@
 import numpy as np
 import cv2
 import cv2 as cv
-# import quaternion
-import  pyquaternion as  quaternion
+import itertools
+import os
+import struct
+import quaternion
+# import  pyquaternion as  quaternion
 from transforms3d.quaternions import quat2mat, mat2quat
 # from testply import convert_xyzrgb_to_ply,make_pcd
 import open3d as py3d
@@ -13,13 +16,38 @@ from ransac_icp_pointcloud_merge_ob_sag1202 import ransac_icp
 #pip install pyquaternion
 #conda install -c conda-forge quaternion
 # camera intrinsics
-cx = 325.5
-cy = 253.5
-fx = 518.0
-fy = 519.0
+# 1050.3172607421875 0.0 1112.1431884765625
+# 0.0 1050.3172607421875 656.2730102539062
+# 0.0 0.0 1.0
 depthScale = 1000.0
+
+cx = 1112.1
+cy = 656.2
+fx = 1050.3
+fy = 1050.3
 #-- Step 1: Detect the keypoints using SURF Detector
 # https://pystyle.info/opencv-feature-matching/
+
+def convert_zed2pcd_to_ply(zed_pcd):
+    zed_points = zed_pcd[:, :, :3]
+    zed_colors = zed_pcd[:, :, 3]
+    points, colors = [], []
+    for x, y in itertools.product(
+            [a for a in range(zed_colors.shape[0])],
+            [a for a in range(zed_colors.shape[1])]):
+        if zed_points[x, y].sum() == 0. and zed_points[x, y].max() == 0.:
+            continue
+        if np.isinf(zed_points[x, y]).any() or np.isnan(zed_points[x, y]).any():
+            continue
+        # color
+        tmp = zed_depthfloat_to_abgr(zed_colors[x, y])
+        tmp = [tmp[3], tmp[2], tmp[1]]  # ABGR to RGB
+        tmp = np.array(tmp).astype(np.float64) / 255.  # for ply (color is double)
+        colors.append(tmp)
+        # point
+        tmp = np.array(zed_points[x, y]).astype(np.float64)
+        points.append(tmp)
+    return make_pcd(points, colors)
 def computeKeyPointsAndDesp(src):
     minHessian = 400
     detector = cv.xfeatures2d_SURF.create(hessianThreshold=minHessian)
@@ -50,9 +78,12 @@ def computeKeyPointsAndDesp1(img):
     # imshow(dst)
     # cv.show()
 #https://pystyle.info/opencv-feature-matching/
-def computeKeyPointsAndMaches(img1,img2):
+def computeKeyPointsAndMaches_ORB(img1,img2):
+    # img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    # img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
     # OBR 特徴量検出器を作成する。
-    detector = cv2.ORB_create()
+    detector = cv2.ORB_create(nfeatures=2000)
+    # detector = cv2.xfeatures2d.SURF_create()
 
     # 特徴点を検出する。
     kp1, desc1 = detector.detectAndCompute(img1, None)
@@ -66,7 +97,7 @@ def computeKeyPointsAndMaches(img1,img2):
 
     # レシオテストを行う。
     good_matches = []
-    thresh = 0.99
+    thresh = 0.8
     for first, second in matches:
         if first.distance < second.distance * thresh:
             good_matches.append(first)
@@ -93,21 +124,6 @@ def make_pcd(points, colors):
     pcd.points = py3d.utility.Vector3dVector(points)
     pcd.colors = py3d.utility.Vector3dVector(colors)
     return pcd
-def image2PointCloud(rgb, depth):
-   points=[]
-   colors=[]
-   r,c =rgb.shape[:2]
-   for i in range(r):
-       for j in range(c):
-           d=depth[i,j]
-           if d==0:
-               continue
-           p=point2dTo3d([i,j,d])
-           points.append(p)
-           b,g,r=rgb[i,j]/255.0
-           colors.append([r,g,b])
-   return make_pcd(points, colors)
-
 
 def merge_points_cloud(clouds):
     points=[]
@@ -118,37 +134,57 @@ def merge_points_cloud(clouds):
         points.extend(ps)
         colors.extend(cs)
     return make_pcd(points,colors)
-def point2dTo3d(point):
-    z=point[2]/depthScale
-    x=(point[0]-cx)*z/fx
-    y=(point[1]-cy)*z/fy
-    return [x,y,z]
-def get_good_matches_pts(depth1,depth2,rgb1,rgb2,goodMatches,kp1,kp2):
+
+def zed_depthfloat_to_abgr(f):
+    """
+    ZED pcd data format:
+      ----------------------------------------------------------
+      https://www.stereolabs.com/docs/depth-sensing/using-depth/
+      ----------------------------------------------------------
+      The point cloud stores its data on 4 channels using 32-bit
+      float for each channel.
+      The last float is used to store color information, where
+      R, G, B, and alpha channels (4 x 8-bit) are concatenated
+      into a single 32-bit float.
+  """
+    # https://stackoverflow.com/questions/23624212/how-to-convert-a-float-into-hex/38879403
+    if f == 0.:
+        return [0, 0, 0, 0]
+    else:
+        h = hex(struct.unpack('<I', struct.pack('<f', f))[0])
+        return [eval('0x' + a + b) for a, b in zip(h[::2], h[1::2]) if a + b != '0x']
+def get_good_matches_pts_ply(pcd1,pcd2,goodMatches,kp1,kp2):
+    zed_points1 = pcd1[:, :, :3]
+    zed_colors1 = pcd1[:, :, 3]
+    zed_points2 = pcd2[:, :, :3]
+    zed_colors2 = pcd2[:, :, 3]
     pts_obj1=[]
     pts_obj2=[]
     colors_obj1=[]
     colors_obj2=[]
     pts_img=[]
     for i, goodMatch in enumerate(goodMatches):
-        p=np.array(kp1[goodMatch.queryIdx].pt,dtype=np.int)
-        d=depth1[p[1],p[0]]
+        p1=np.array(kp1[goodMatch.queryIdx].pt,dtype=np.int)
+        d1=zed_points1[p1[1],p1[0]][2]
         p2=np.array(kp2[goodMatch.trainIdx].pt,dtype=np.int)
-        d2=depth2[p2[1],p2[0]]
-        if d == 0 or d2 ==0:
+        d2=zed_points2[p2[1],p2[0]][2]
+        if d1 == 0 or d2 ==0:
             continue;
         pts_img.append(kp2[goodMatch.trainIdx].pt)
-        pt=[p[0],p[1],d]
-        pd = point2dTo3d(pt)
-        # 将(u,v,d)转成(x,y,z)
-        pts_obj1.append(pd)
-        pt2=[p2[0],p2[1],d2]
-        pd2 = point2dTo3d(pt2)
-        pts_obj2.append(pd2)
 
-        b, g, r = rgb1[p[1],p[0]] / 255.0
-        colors_obj1.append([r, g, b])
-        b, g, r = rgb2[p2[1],p2[0]] / 255.0
-        colors_obj2.append([r, g, b])
+        pts_obj1.append(zed_points1[p1[1],p1[0]])
+        pts_obj2.append(zed_points2[p2[1],p2[0]])
+
+        tmp = zed_depthfloat_to_abgr(zed_colors1[p1[1],p1[0]])
+        tmp = [tmp[3], tmp[2], tmp[1]]  # ABGR to RGB
+        tmp = np.array(tmp).astype(np.float64) / 255.  # for ply (color is double)
+        colors_obj1.append(tmp)
+
+        tmp = zed_depthfloat_to_abgr(zed_colors2[p2[1],p2[0]])
+        tmp = [tmp[3], tmp[2], tmp[1]]  # ABGR to RGB
+        tmp = np.array(tmp).astype(np.float64) / 255.  # for ply (color is double)
+        colors_obj2.append(tmp)
+
     if len(pts_obj1)==0 or len(pts_img)==0:
         return -1
     print(f"pts_obj1:{len(pts_obj1)}",f"pts_obj2:{len(pts_obj2)}")
@@ -168,46 +204,15 @@ def get_good_matches_pts(depth1,depth2,rgb1,rgb2,goodMatches,kp1,kp2):
     distCoeffs = np.array(distCoeffs, dtype=np.float64)
 
     _, rvec0, T0 = cv2.solvePnP(pts_obj, pts_img, cameraMatrix, distCoeffs=distCoeffs)
-    # rvec0 = np.array(ini_pose[i, 0:3].cpu().view(3, 1))
-    # T0 = np.array(ini_pose[i, 3:6].cpu().view(3, 1))
+
     _, rVec, tVec = cv2.solvePnP(pts_obj, pts_img, cameraMatrix, distCoeffs=None,
                                  flags=cv.SOLVEPNP_ITERATIVE, useExtrinsicGuess=False, rvec=rvec0, tvec=T0)
     #https://www.366service.com/jp/qa/ed8c0298cc30a02ee80c3d9ecef63a69
     rotM = cv2.Rodrigues(rVec)[0]
-    # rotM=np.array(rotM).T
-    # rotM = -np.array(rotM).T * np.array(tVec)
-    # rotation_mat, _ = cv2.Rodrigues(rVec)
     pose_mat = cv2.hconcat((rotM, tVec))
     #https://programtalk.com/python-examples/cv2.solvePnP/
     pose_mat=cv2.vconcat((pose_mat,np.array([[0.0,0.0,0.0,1.0]])))
     return pose_mat,translst
-def main_sample():
-    bp='data/'
-    rgb1=cv2.imread(f"./{bp}/rgb1.png")
-    rgb2=cv2.imread(f"./{bp}/rgb2.png")
-    depth1=cv2.imread(f"./{bp}/depth1.png")[:,:,0]
-    depth2=cv2.imread(f"./{bp}/depth2.png")[:,:,0]
-
-    kp1, kp2, goodMatches ,dst= computeKeyPointsAndMaches(rgb1, rgb2)
-    cv2.imwrite("data/dst2.jpg", dst)
-    pose_mat, translst = get_good_matches_pts(depth1,depth2,rgb1,rgb2,goodMatches,kp1,kp2)
-    cloud1 = image2PointCloud(rgb1, depth1)
-    cloud2 = image2PointCloud(rgb2, depth2)
-
-    py3d.io.write_point_cloud('pcd_merge_org1.ply', cloud1)
-    py3d.io.write_point_cloud('pcd_merge_org2.ply', cloud2)
-
-    cloud1t_r = cloud1.transform(translst[0])
-    cloud2t_r = cloud2.transform(translst[1])
-    py3d.io.write_point_cloud('pcd_merge_1t_ransac.ply', cloud1t_r)
-    py3d.io.write_point_cloud('pcd_merge_2t_ransac.ply', cloud2t_r)
-
-    cloud2t = cloud2.transform(pose_mat)
-    cloud1t = cloud1.transform(pose_mat) # good quanlity
-    py3d.io.write_point_cloud('pcd_merge_1t.ply', cloud1t)
-    py3d.io.write_point_cloud('pcd_merge_2t.ply', cloud2t)
-    # py3d.io.write_point_cloud('pcd_merge.ply', pcd)
-import os
 
 def get_absolute_file_paths(directory,fn='image.npy'):
    fils_list=[]
@@ -222,11 +227,17 @@ def get_merge_fns(basep,i):
     fn_rgb='image_org_mergeid_%04d.png'%(i)
     fn_msk='moving_detection_mask_mergeid_%04d.npy'%(i)
     fn_rgb_msk='rgb_mask_mergeid_%04d.png'%(i)
+    #depth_mask_mergeid_0000
+    fn_depth='depth_mask_mergeid_%04d.npy'%(i)
+    fn_pcd_msk='pcd_mask_mergeid_%04d.npy'%(i)
     ply_list = get_absolute_file_paths(basep,  fn=fn_merge)
     rgb_list = get_absolute_file_paths(basep,  fn=fn_rgb)
     msk_list = get_absolute_file_paths(basep,  fn=fn_msk)
     rgb_msk_list = get_absolute_file_paths(basep,  fn=fn_rgb_msk)
-    return ply_list,rgb_list,msk_list,rgb_msk_list
+    depth_list = get_absolute_file_paths(basep,  fn=fn_depth)
+    pcd_list = get_absolute_file_paths(basep,  fn=fn_pcd_msk)
+
+    return ply_list,rgb_list,msk_list,rgb_msk_list,depth_list,pcd_list
 def get_msk_fns(basep,i):
     fn_merge='pcd_mask_mergeid_%04d.ply'%(i)
     fn_rgb='image_org_mergeid_%04d.png'%(i)
@@ -261,8 +272,23 @@ def getbbox_msk(img_msk):
             ce=i
             break
     return rs,re,cs,ce
+def convert_bgra2rgba(img):
+    # ZED2 numpy color is BGRA.
+    rgba = np.zeros(img.shape).astype(np.uint8)
+    rgba[:, :, 0] = img[:, :, 2]  # R
+    rgba[:, :, 1] = img[:, :, 1]  # G
+    rgba[:, :, 2] = img[:, :, 0]  # B
+    rgba[:, :, 3] = img[:, :, 3]  # A
+    return rgba
+
+def load_image_from_npy(p, convert=True):
+    color_org = np.load(p)
+    color = convert_bgra2rgba(color_org) if convert else color_org
+    # pil_img = Image.fromarray(color.astype(np.uint8))
+    # pil_img.save(f'{p}/image.png')
+    return color[:, :, :3], color_org
+
 def get_rgb_img_msk(id,bp,po,pose):
-    ri=ransac_icp()
     basep = f'{bp}/{pose}'
     baseout = f'{po}/{pose}'
     os.makedirs(baseout,exist_ok=True)
@@ -279,59 +305,122 @@ def get_rgb_img_msk(id,bp,po,pose):
         fn_rgb=m.replace(fn,'rgb_mask_mergeid_%04d.png'%(id))
         # img_rgb=cv2.cvtColor(img_crop, cv2.COLOR_BGR2RGB)
         cv2.imwrite(fn_rgb,img_crop)
+
+        fn_depth=m.replace(fn,'depth.npy')
+        img_dep=np.load(fn_depth)
+        img_dep[img_dep == -np.inf] = 0
+        img_dep[img_dep == np.inf] = 0
+        dep_msk=img_dep*img_msk
+
+        dep_crop=dep_msk[rs:re,cs:ce]
+        fn_rgb=m.replace(fn,'depth_mask_mergeid_%04d.npy'%(id))
+        np.save(fn_rgb,dep_crop)
+
+
+        fn_pcd=m.replace(fn,'pcd.npy')
+        pcd=np.load(fn_pcd)
+        # img_image[img_image == -np.inf] = 0
+        # img_image[img_image == np.inf] = 0
+        img_msk4=np.concatenate([msk1,msk1,msk1,msk1],axis=2)
+        pcd_msk=pcd*img_msk4
+
+        pcd_crop=pcd_msk[rs:re,cs:ce]
+        fn_rgb=m.replace(fn,'pcd_mask_mergeid_%04d.npy'%(id))
+        np.save(fn_rgb,pcd_crop)
+
+        # ply1 = convert_zed2pcd_to_ply(pcd_crop)
+
+        # py3d.io.write_point_cloud(f"data/pcd_org1_%04d.ply" % (id), ply1)
+        #image.npy
         print(fn_rgb)
+
+#https://www.analyticsvidhya.com/blog/2019/10/detailed-guide-powerful-sift-technique-image-matching-python/
+def computeKeyPointsAndMachesSift(img1,img2):
+    # OBR 特徴量検出器を作成する。
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    # sift
+    detector = cv2.xfeatures2d.SIFT_create()
+    # 特徴点を検出する。
+    kp1, desc1 = detector.detectAndCompute(img1, None)
+    kp2, desc2 = detector.detectAndCompute(img2, None)
+    # マッチング器を作成する。
+    # bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+    bf = cv.BFMatcher()
+    # マッチングを行う。
+    matches = bf.knnMatch(desc1, desc2, k=2)
+    # matches = bf.match(desc1, desc2)
+    # レシオテストを行う。
+    # matches = sorted(matches, key=lambda x: x.distance)
+    good_matches = []
+    thresh = 0.9
+    # good_matches=matches[:50]
+    for first, second in matches:
+        if first.distance < second.distance * thresh:
+            good_matches.append(first)
+    print("good_matches:",len(good_matches))
+    # マッチング結果を描画する。
+    dst = cv2.drawMatches(img1, kp1, img2, kp2, good_matches, None)
+    # cv2.imwrite("data/dst2.jpg",dst)
+    return kp1,kp2,good_matches,dst
 def merge_rgb_img_msk(id,bp,po,pose):
     basep = f'{bp}/{pose}'
     baseout = f'{po}/{pose}'
     os.makedirs(baseout,exist_ok=True)
-    ply_list,rgb_list,msk_list,rgb_msk_list=get_merge_fns(basep, id)
+    ply_list,rgb_list,msk_list,rgb_msk_list,depth_list,pcd_list=get_merge_fns(basep, id)
     fn_rgb1,fn_rgb2,_=rgb_msk_list
 
     rgb1=cv2.imread(fn_rgb1)
     rgb2=cv2.imread(fn_rgb2)
-    # rgb1 = cv2.medianBlur(rgb1, 3)
-    # rgb2 = cv2.medianBlur(rgb2, 3)
-    # rgb2 = cv2.cvtColor(rgb2, cv2.COLOR_BGR2GRAY)
-    # rgb1 = cv2.cvtColor(rgb1, cv2.COLOR_BGR2GRAY)
-    kp1, kp2, goodMatches ,dst= computeKeyPointsAndMaches(rgb1, rgb2)
+    # rgb_grey_1 = cv2.cvtColor(rgb1, cv2.COLOR_BGR2GRAY)
+    # rgb_grey_2 = cv2.cvtColor(rgb2, cv2.COLOR_BGR2GRAY)
+    # ret, rgb_grey_1 = cv2.threshold(rgb_grey_1, 0, 255, cv2.THRESH_OTSU)
+    # ret, rgb_grey_2 = cv2.threshold(rgb_grey_2, 0, 255, cv2.THRESH_OTSU)
+
+    kp1, kp2, goodMatches ,dst= computeKeyPointsAndMaches_ORB(rgb1, rgb2)
+    # kp1, kp2, goodMatches ,dst= computeKeyPointsAndMachesSift(rgb1, rgb2)
     cv2.imwrite(f"{basep}/dsp_%04d.jpg"%(id),dst)
+    '''
+    pcd1=np.load(pcd_list[0])
+    pcd2=np.load(pcd_list[1])
+    # ply1=convert_zed2pcd_to_ply(pcd1)
+    # py3d.io.write_point_cloud(f"data/pcd_org1_%04d.ply"%(id), ply1)
+    pose_mat, translst = get_good_matches_pts_ply(pcd1,pcd2,goodMatches,kp1,kp2)
+    cloud1 = py3d.io.read_point_cloud(ply_list[0])
+    cloud2 = py3d.io.read_point_cloud(ply_list[1])
 
-    # pcds = ri.load_pcds(files_list)
+    py3d.io.write_point_cloud(f"{basep}/pcd_org1_%04d.ply"%(id), cloud1)
+    py3d.io.write_point_cloud(f"{basep}/pcd_org2_%04d.ply"%(id), cloud2)
 
-    # size = np.abs((pcds[0].get_max_bound() - pcds[0].get_min_bound())).max() / 30
-    # pcdsn=[]
-    # for pcd in pcds:
-    #     pcdn=ri.add_color_normal(pcd,size)
-    #     pcdsn.append(pcdn)
-    #
-    # pcd_aligned,translst = ri.align_pcds(pcdsn, size)
-    # all_points = []
-    # all_colors = []
-    # for i, pcd in enumerate(pcd_aligned):
-    #     all_points.append(np.asarray(pcd.points))
-    #     all_colors.append(np.asarray(pcd.colors))
-    #     print("pcd:",i)
-    #     ft_pcd_m = f'{baseout}/pcd_aligned_mergeid_%04d_cam%d.ply'% (id,i)
-    #     py3d.io.write_point_cloud(ft_pcd_m, pcd)
-    # pcd_a_merge=ri.make_pcd(np.vstack(all_points),np.vstack(all_colors))
-    # ft_pcd_m=f'{baseout}/pcd_aligned_mergeid_%04d.ply'% (id)
-    # py3d.io.write_point_cloud(ft_pcd_m, pcd_a_merge)
+    cloud1t_r = cloud1.transform(translst[0])
+    cloud2t_r = cloud2.transform(translst[1])
+    # cloud2t = cloud2.transform(pose_mat)
+    cloud1t = cloud1.transform(pose_mat) # good quanlity
+    py3d.io.write_point_cloud(f"{basep}/pcd_trans1_%04d.ply"%(id), cloud1t)
+    # py3d.io.write_point_cloud(f"{basep}/pcd_trans2_%04d.ply"%(id), cloud2t)
+    pcd_merge=merge_points_cloud([cloud1t,cloud2])
+    py3d.io.write_point_cloud(f"{basep}/pcd_merge_%04d.ply"%(id), pcd_merge)
+
+    pcd_merge=merge_points_cloud([cloud1t_r,cloud2t_r])
+    py3d.io.write_point_cloud(f"{basep}/pcd_ransac_merge_%04d.ply"%(id), pcd_merge)
+    '''
 def preprocess_dt():
-    bp='D:/02_AIPJ/004_ISB/pointclouds_dt/'
-    po='D:/02_AIPJ/004_ISB/pointclouds_dt/'
-    dt_ids = ['d2_a30_h1.5', 'd2_a45_h1.5']
+    bp='data/'
+    po='data/'
+    dt_ids = ['d2_a30_h1.5']#, 'd2_a45_h1.5']
     for dt_id in dt_ids :
         for i in range(4):
             get_rgb_img_msk(i,bp,po,dt_id)
 def merge_dt():
-    bp='D:/02_AIPJ/004_ISB/pointclouds_dt/'
-    po='D:/02_AIPJ/004_ISB/pointclouds_dt/'
-    dt_ids = ['d2_a30_h1.5', 'd2_a45_h1.5']
+    bp='data/'
+    po='data/'
+    dt_ids = ['d2_a30_h1.5']#, 'd2_a45_h1.5']
     for dt_id in dt_ids :
         for i in range(4):
             merge_rgb_img_msk(i,bp,po,dt_id)
+# preprocess_dt()
 merge_dt()
-
 '''
 https://www.learnopencv.com/head-pose-estimation-using-opencv-and-dlib/
 camera pose
